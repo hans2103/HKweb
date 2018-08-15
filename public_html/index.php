51176f4aeca61b7572eab1d7a0c4472b61ce36d7
Hans Kuijpers <info@hkweb.nl>
2018-08-15 16:52:43 +0200

    playing around with npm scripts

---
+++ public_html/index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link href="//www.googletagmanager.com" rel="dns-prefetch">
+  <title>Building sites with Joomla! and Magento &mdash; HKweb.nl</title>
+  <meta name="description" content="Building sites with Joomla! and Magento. HKweb.nl is the website from frontend developer Hans Kuijpers. ">
+  <link rel="shortcut icon" href="/favicon.ico"/>
+  <meta content="Building sites with Joomla! and Magento &mdash; HKweb.nl" property="og:title">
+  <meta content="Building sites with Joomla! and Magento &mdash; HKweb.nl" name="twitter:title">
+  <meta content="website" property="og:type">
+  <meta content="summary" name="twitter:card">
+  <meta content="HKweb.nl" property="og:site_name">
+  <meta content="@hans2103" name="twitter:site">
+  <meta content="@hans2103" name="twitter:creator">
+  <link href="101700795462513172784" rel="publisher">
+  <link href="https://plus.google.com/101700795462513172784/posts" rel="author">
+  <meta name="p:domain_verify" content="f847bdacf83eecd67f0641fb3a164102"/>
+  <meta content="https://hkweb.nl/" property="og:url">
+  <meta content="https://hkweb.nl/" name="twitter:url">
+  <meta property="og:image" content="https://hkweb.nl/images/hkweb-logo.png">
+  <meta name="twitter:image:src" content="https://hkweb.nl/images/hkweb-logo.png">
+  <meta property="og:image:type" content="image/png">
+  <meta property="og:image:width" content="1024">
+  <meta property="og:image:height" content="1024">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="canonical" href="https://hkweb.nl/">
+  <script type="text/javascript">
+    !function(){"use strict";function e(e,t,n){e.addEventListener?e.addEventListener(t,n,!1):e.attachEvent&&e.attachEvent("on"+t,n)}function t(e){return window.localStorage&&localStorage.font_css_cache&&localStorage.font_css_cache_file===e}function n(){if(window.localStorage&&window.XMLHttpRequest)if(t(o))c(localStorage.font_css_cache);else{var n=new XMLHttpRequest;n.open("GET",o,!0),e(n,"load",function(){4===n.readyState&&(c(n.responseText),localStorage.font_css_cache=n.responseText,localStorage.font_css_cache_file=o)}),n.send()}else{var a=document.createElement("link");a.href=o,a.rel="stylesheet",a.type="text/css",document.getElementsByTagName("head")[0].appendChild(a),document.cookie="font_css_cache"}}function c(e){var t=document.createElement("style");t.innerHTML=e,document.getElementsByTagName("head")[0].appendChild(t)}var o="/css/font.css";window.localStorage&&localStorage.font_css_cache||document.cookie.indexOf("font_css_cache")>-1?n():e(window,"load",n)}();
+  </script>
+    </head>
+<body>
+
+<div class="svg-sprite">
+  <?php include_once 'images/icons/sprite.svg'; ?>
+</div>
+
+
+<nav class="links">
+  <a href="#section-seen">1</a>
+  <a href="#section-whois">2</a>
+  <a href="#section-coding">3</a>
+  <a href="#section-testing">4</a>
+  <a href="#section-training">5</a>
+  <a href="#section-sharing">6</a>
+</nav>
+
+<div class="docScroller">
+  <section id="section-seen" class="section section--seen">
+    <header class="section__header">
+      <h2 class="section__title section__title--hero">Have you seen Hans?</h2>
+    </header>
+  </section>
+
+  <section id="section-whois" class="section section--whois">
+    <header class="section__header">
+      <h2 class="section__title">Who is Hans?</h2>
+    </header>
+    <div class="section__content">
+      <h3>Hans Kuijpers</h3>
+      <p>Curabitur rhoncus nisl at est iaculis consectetur. Praesent eget justo non nisl auctor ullamcorper. Morbi
+        pellentesque pulvinar ante quis eleifend. Nullam pharetra est sodales lacinia sagittis. Maecenas sodales, odio
+        aliquet viverra fringilla, lorem augue lacinia augue, in sodales enim mi et enim. Donec nunc ipsum, luctus ut
+        auctor in, bibendum elementum purus. Sed luctus, odio non consectetur tempus, tellus ipsum vehicula velit, sed
+        condimentum felis ligula venenatis eros. Proin orci neque, euismod sed massa at, luctus sollicitudin libero!</p>
+    </div>
+    <div class="section__content">
+      <h3>Hans Kuijpers is a ... </h3>
+      <ul>
+        <li>Front-end developer</li>
+        <li>Site Performance Expert</li>
+        <li>Joomla! and Magento Trainer</li>
+        <li>Blogger</li>
+      </ul>
+    </div>
+  </section>
+
+  <section id="section-coding" class="section section--coding">
+    <header class="section__header">
+      <h2 class="section__title">Hans is coding</h2>
+    </header>
+    <div class="section__content">
+      <h3>Joomla!, Magento and WordPress Front-end developer</h3>
+      <p>Although this page was built using Jekyll most of the time I work with Joomla! and Magento (1.9.x and 2.x) and
+        sometimes WordPress. The CMS I use depends on the needs of the project. A lot of functionality can be done with
+        the core of each and every CMS.</p>
+      <p>Proin adipiscing massa lorem, nec consectetur neque auctor sed. Nam volutpat, tellus sed aliquet condimentum,
+        erat urna sollicitudin tellus, lacinia rutrum magna ipsum in urna. Phasellus quis semper elit. Pellentesque
+        egestas vehicula faucibus.</p>
+      <p>Pellentesque convallis, felis vel egestas gravida, ligula est aliquet ante, a eleifend nulla nulla.</p>
+      <p><a href="https://www.behance.net/hans2103" class="anchor--button">See his work on Behance</a></p>
+    </div>
+    <figure>
+      <svg><use href="#hans-coding" /></svg>
+    </figure>
+  </section>
+
+  <section id="section-testing" class="section section--testing">
+    <header class="section__header">
+      <h2 class="section__title">Hans is testing</h2>
+    </header>
+    <div class="section__content">
+      <h3>Fast websites are important</h3>
+      <p>Mauris in vehicula quam, a adipiscing diam. Vivamus ut imperdiet lectus. Etiam vitae scelerisque magna, ac
+        dignissim lorem. Nam scelerisque facilisis arcu, eu egestas eros sollicitudin at. Curabitur porttitor
+        tristique augue, eget semper nisi. Praesent dapibus pretium massa, at congue neque iaculis et. Quisque nisl
+        mi, ultricies id quam ut, cursus condimentum lectus.</p>
+      <p>Vivamus molestie neque turpis, eu sagittis tellus tincidunt in. Etiam rhoncus interdum nunc non feugia.</p>
+      <h3>Professional services</h3>
+      <p>Configuration and running testPage loading timeReal life traffic simulationOptimization advice</p>
+      <p><a href="#contact-hans" rel="modal:open" class="anchor--button">Contact Hans for a test</a></p>
+    </div>
+    <figure>
+      <svg><use href="#hans-testing" /></svg>
+    </figure>
+  </section>
+
+  <section id="section-training" class="section section--training">
+    <header class="section__header">
+      <h2 class="section__title">Hans is training</h2>
+    </header>
+    <div class="section__content">
+      <div>
+        <h3>Magento training</h3>
+        <p>Ut eleifend sed velit nec interdum. Integer porttitor tristique nibh, in molestie nunc consectetur non.
+          Praesent massa nisi, porta quis elit sed, tincidunt blandit neque. In hac habitasse platea dictumst. Aenean
+          eget est at enim aliquet!</p>
+      </div>
+      <div>
+        <h3>Joomla! training</h3>
+        <p>Ut eleifend sed velit nec interdum. Integer porttitor tristique nibh, in molestie nunc consectetur non.
+          Praesent massa nisi, porta quis elit sed, tincidunt blandit neque. In hac habitasse platea dictumst. Aenean
+          eget est at enim aliquet!</p>
+      </div>
+      <p><a href="#contact-hans" rel="modal:open" class="anchor--button">Ask Hans to train you</a></p>
+    </div>
+    <figure>
+      <svg><use href="#hans-training" /></svg>
+    </figure>
+  </section>
+
+  <section id="section-sharing" class="section section--sharing">
+    <header class="section__header">
+      <h2 class="section__title">Hans is sharing</h2>
+    </header>
+    <div class="section__content">
+      <div>
+        <h3>Passion to share</h3>
+        <p>Together with a lot of other frontend developers I organise or help organising events to share knowledge.</p>
+        <ul>
+          <li><a href="http://joomladagen.nl/">Joomla! Day Netherlands</a></li>
+          <li><a href="https://magento2seminar.yireo.com/">Magento 2 Seminar</a></li>
+          <li><a href="http://www.meetup.com/Joomla-Nederland/">Dutch Joomla User Groups</a></li>
+          <li><a href="http://www.meetup.com/Magento-User-Group-Brabant/">Dutch Magento User Groups</a></li>
+        </ul>
+        <h3>Knowledge to share</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus libero sit amet nisl euismod, a fringilla
+          arcu hendrerit. Fusce Slideshare arcu a turpis rutrum feugiat. Pellentesque lobortis, tortor a pellentesque
+          blandit, ligula arcu condimentum sem.</p>
+      </div>
+      <div>
+        <h3>Tips and tricks to share</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus libero:</p>
+        <dl>
+          <dt>20 Februari 2014</dt>
+          <dd><a href="https://www.byte.nl/blog/tips-successful-magento-site-3-google-analytics-views">Tips for a
+            Successful Magento Site #3 – Google Analytics Views</a></dd>
+          <dt>30 Januari 2014</dt>
+          <dd><a href="https://www.byte.nl/blog/tips-successful-magento-site-2-google-analytics-filters">Tips for a
+            successful Magento site #2 – Google Analytics filters</a></dd>
+          <dt>3 Januari 2014</dt>
+          <dd><a href="https://www.byte.nl/blog/magento-blok-verplaatsen">Magento tricks - een blok verplaatsen</a></dd>
+          <dt>17 December 2013</dt>
+          <dd><a href="https://www.byte.nl/blog/tips-successful-magento-site-1-search-engines">Tips for a successful
+            Magento site #1 – search engines</a></dd>
+          <dt>26 april 2013</dt>
+          <dd><a href="https://www.byte.nl/blog/magento-rich-snippets-made-easy">Magento Rich Snippets made easy</a>
+          </dd>
+        </dl>
+      </div>
+    </div>
+    <figure>
+      <svg><use href="#hans-sharing" /></svg>
+    </figure>
+    <footer class="section__footer">
+      <div class="section__content">
+        <div class="site-footer--social social-icon--list">
+          <a class="social-icon--link facebook" href="https://www.facebook.com/hans2103"><svg><use href="#facebook" /></svg><span class="social-icon--title">Facebook</span></a>
+          <a class="social-icon--link twitter" href="https://twitter.com/hans2103"><svg><use href="#twitter" /></svg><span class="social-icon--title">Twitter</span></a>
+          <a class="social-icon--link googleplus" href="https://plus.google.com/+HansKuijpers"><svg><use href="#google-plus" /></svg><span class="social-icon--title">Googleplus</span></a>
+          <a class="social-icon--link linkedin" href="https://www.linkedin.com/in/hans2103"><svg><use href="#linkedin-in" /></svg><span class="social-icon--title">LinkedIn</span></a>
+          <a class="social-icon--link behance" href="https://www.behance.net/hans2103"><svg><use href="#behance" /></svg><span class="social-icon--title">Behance</span></a>
+          <a class="social-icon--link github" href="https://github.com/hans2103"><svg><use href="#github" /></svg><span class="social-icon--title">GitHub</span></a>
+          <a class="social-icon--link slideshare" href="http://www.slideshare.net/hans2103"><svg><use href="#slideshare" /></svg><span class="social-icon--title">Slideshare</span></a>
+        </div>
+        <div class="site-footer--copyright">
+          <p>&copy; HKweb.nl 2005<?php echo(date('Y') != 2017 ? ' - ' . date('Y') : ''); ?>. All rights Reserverd. Designed by <a
+          href="http://untilsunday.it/">Until Sunday</a>. Developed by <a href="https://hkweb.nl">Hans Kuijpers</a>.
+        </p>
+        </div>
+      </div>
+    </footer>
+  </section>
+</div>
+
+
+<script defer src="js/app.min.js"></script>
+</body>
+</html>

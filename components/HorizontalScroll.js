d6bab17928ccf6a0d439dd30d6ef11c555687ea8
Hans Kuijpers <hans2103@gmail.com>
2020-12-28 14:42:24 +0100

    :wastebasket: clean sweep

--- components/HorizontalScroll.js
+++
@@ -1,8 +0,0 @@
-// import styled from 'styled-components'
-// import { Flex } from './Flex'
-//
-// export const HorizontalScroll = styled(Flex)`
-//   -webkit-overflow-scrolling: touch;
-//   overflow-x: scroll;
-//   flex-wrap: nowrap;
-// `;

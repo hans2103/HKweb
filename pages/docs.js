55e34cd14a6acd802d19fb5ad199b9c972095b24
Hans Kuijpers <hans2103@gmail.com>
2020-12-29 12:42:58 +0100

    add reat-styleguidist

---
+++ pages/docs.js
@@ -0,0 +1,8 @@
+// pages/docs.js
+
+const template = '<div id="rsg-root"></div><script src="/build/bundle.js"></script>'
+const Docs = () => (
+    <div dangerouslySetInnerHTML={{ __html: `${template}` }} />
+)
+
+export default Docs

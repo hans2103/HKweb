cbe8a238f9668eb45d1403c369fb7fadafd4b069
Hans Kuijpers <hans2103@gmail.com>
2020-12-30 20:40:25 +0100

    extend image component

--- src/components/image.js
+++ src/components/image.js
@@ -1,9 +1,18 @@
 // src/components/image.js
 
-import styled from 'styled-components';
 import NextImage from 'next/image';
+import propTypes from '@styled-system/prop-types';
+import PropTypes from 'prop-types';
 
-const Image = styled(NextImage)``;
+const Image = ({ src, alt, ...props }) => {
+    return <NextImage src={src} alt={alt} {...props} />;
+};
+
+Image.propTypes = {
+    ...propTypes.space,
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+};
 
 /** @component */
 export default Image;

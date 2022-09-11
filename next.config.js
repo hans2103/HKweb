a5674b7064c35c0939954a17b96bab1a9e486c47
Hans Kuijpers <hans2103@gmail.com>
2022-09-11 15:27:48 +0200

    :lock: add security headers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,33 @@
 module.exports = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value:
+                            'default-src \'self\'; font-src \'self\'; img-src \'self\' ik.imagekit.io; script-src \'self\''
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY'
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff'
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'origin-when-cross-origin'
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'camera=(); battery=(self); geolocation=(); microphone=()'
+                    }
+                ]
+            }
+        ];
+    }
 };
